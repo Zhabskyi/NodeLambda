c1facedc6bd64615ea3043f5c78fe3417cc7dcd8
Zhabskyi <poltavagaz@gmail.com>
2024-05-02 10:42:46 -0600

    infrastructure

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Lambda!"
+      message: "Hello from Lambda! v1.0"
     }),
     headers: {
       "Content-Type": "application/json"
