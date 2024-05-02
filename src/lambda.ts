782227fcd4f4e9017a147a0e28abf793a126bfbe
Zhabskyi <poltavagaz@gmail.com>
2024-05-02 13:26:52 -0600

    infrastructure

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,12 +1,25 @@
-exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Hello from Lambda! v1.0"
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
-  return response;
+export const handler = async (event: any) => {
+  try {
+    const response: any = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Hello from Lambda! v1.0"
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    return response;
+  } catch (error) {
+    console.error(error); // Log the error to CloudWatch
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "An error occurred"
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+  }
 };
